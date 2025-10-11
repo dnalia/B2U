d2e4c74457059e10781f1b6a57fce9fcccad94ab
izzatiznl <izzatizainal005@gmail.com>
2025-10-11 21:43:57 +0800

    Update models.py

--- Intern Project B2U/projectintern/inventorySystem/models.py
+++ Intern Project B2U/projectintern/inventorySystem/models.py
@@ -29,3 +29,10 @@ class AssetUpdate(models.Model):
 
     def __str__(self):
         return f"{self.engineer_name} - {self.location} - {self.status}"
+
+class User(models.Model):
+    username = models.CharField(max_length=100, unique=True)
+    password = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.username
