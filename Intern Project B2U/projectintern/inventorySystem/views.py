b1f2d84d6f82f5d4da79e336fd69044a79961ab2
izzatiznl <izzatizainal005@gmail.com>
2025-10-11 23:00:59 +0800

    login page.html

--- Intern Project B2U/projectintern/inventorySystem/views.py
+++ Intern Project B2U/projectintern/inventorySystem/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
 
-# Create your views here.
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user:
+            login(request, user)
+            return redirect('/')  # home page selepas login
+        else:
+            messages.error(request, "Username atau password salah")
+    return render(request, 'login.html')
+
+def homepage(request):
+    return render(request, 'homepage.html') 
