37bfe21d2a7c72a0c46d87b0a2eb69e379df8b91
hannahbtrisya <hannahbtrisya@gmail.com>
2025-10-11 15:28:35 +0800

    Added AssetUpdate model

--- Intern Project B2U/projectintern/inventorySystem/models.py
+++ Intern Project B2U/projectintern/inventorySystem/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+class AssetUpdate(models.Model):
+    engineer_name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    type = models.CharField(max_length=100)  # e.g. Asset Refresh
+    user_name = models.CharField(max_length=100)
+
+    old_pc_barcode = models.CharField(max_length=100, blank=True, null=True)
+    old_monitor_barcode = models.CharField(max_length=100, blank=True, null=True)
+
+    new_pc_barcode = models.CharField(max_length=100, blank=True, null=True)
+    new_pc_serial_no = models.CharField(max_length=100, blank=True, null=True)
+    new_monitor_barcode_no = models.CharField(max_length=100, blank=True, null=True)
+    new_monitor_serial_no = models.CharField(max_length=100, blank=True, null=True)
+
+    old_ip_address = models.CharField(max_length=50, blank=True, null=True)
+    new_ip_address = models.CharField(max_length=50, blank=True, null=True)
+    new_mac_address = models.CharField(max_length=100, blank=True, null=True)
+
+    start_time = models.CharField(max_length=50)
+    end_time = models.CharField(max_length=50)
+
+    status = models.CharField(max_length=50)
+    remarks = models.TextField(blank=True, null=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.engineer_name} - {self.location} - {self.status}"
